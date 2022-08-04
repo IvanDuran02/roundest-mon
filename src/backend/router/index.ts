89a79d1b688595e9371d177bdfa7db910382e9bc
Ivan Duran <ivanduran@Ivans-MBP.lan>
2022-08-03 23:46:37 -0400

    backend with pokemon data

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,19 +1,14 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
+import { PokemonClient } from "pokenode-ts";
 
-export const appRouter = trpc.router().query("hello", {
-  // router with a query hello
-  input: z
-    .object({
-      // the query takes in a text that is nullable
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      // and returns a greeting with text, if no text return hello, world
-      greeting: `hello ${input?.text ?? "world"}`,
-    };
+export const appRouter = trpc.router().query("get-pokemon-by-id", {
+  input: z.object({ id: z.number() }),
+  async resolve({ input }) {
+    const api = new PokemonClient();
+
+    const pokemon = await api.getPokemonById(input.id);
+    return pokemon;
   },
 });
 
